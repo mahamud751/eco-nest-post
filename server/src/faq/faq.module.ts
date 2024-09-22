2ec9b208afb2cabfba642aac11add35461672678
Mahamud Pino <mahamudpino24678@gmail.com>
2024-09-22 17:36:35 +0600

    add faq

--- server/src/faq/faq.module.ts
+++ server/src/faq/faq.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { FaqService } from './faq.service';
 import { FaqController } from './faq.controller';
+import { AuditLogService } from 'src/audit/audit.service';
 
 @Module({
   imports: [],
   controllers: [FaqController],
-  providers: [FaqService, PrismaService],
+  providers: [FaqService, PrismaService, AuditLogService],
 })
 export class FaqModule {}
