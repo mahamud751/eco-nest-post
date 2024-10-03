3d7b369a7a42ba016ec14dd42ea7aef98199e1c1
Mahamud Pino <mahamudpino24678@gmail.com>
2024-10-04 00:24:54 +0600

    add

--- client/src/app/redux/store.tsx
+++ client/src/app/redux/store.tsx
@@ -1,14 +1,9 @@
 import { createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "./reducers"; // Import your combined reducers
+import { composeWithDevTools } from "@redux-devtools/extension";
+import rootReducer from "./reducers";
 
 const initialStore = {};
 
-// Create the store without `redux-thunk`
-const store = createStore(
-  rootReducer,
-  initialStore,
-  composeWithDevTools() // Still use dev tools, just no middleware
-);
+const store = createStore(rootReducer, initialStore, composeWithDevTools());
 
 export default store;
