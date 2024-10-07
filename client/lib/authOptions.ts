3b9439e04aedd00e9286eeb5a14545602ba0b42c
mahamud751 <mahamudpino24678@gmail.com>
2024-10-07 11:22:06 +0000

    add

--- client/lib/authOptions.ts
+++ client/lib/authOptions.ts
@@ -9,6 +9,11 @@ export const authOptions: NextAuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            authorization: {
+                params: {
+                    scope: "openid email profile",
+                },
+            },
         }),
     ],
     callbacks: {
