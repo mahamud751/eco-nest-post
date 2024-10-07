9a24e4dfcc9a9bf31a57573804d3792856131a35
mahamud751 <mahamudpino24678@gmail.com>
2024-10-07 11:56:41 +0000

    add

--- client/lib/authOptions.ts
+++ client/lib/authOptions.ts
@@ -44,7 +44,9 @@ export const authOptions: NextAuthOptions = {
         async session({ session, token }) {
             if (session?.user) {
                 // If you need to enrich the session object, do so here
-                //@ts-ignore
+                //@ts-expect-error
+
+
                 session.user.id = token.sub;
                 return session; // Return the updated session object
             }
