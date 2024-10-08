d73327a6ab8def6d4e2c2aac4c61a7822790b128
Mahamud Pino <mahamudpino24678@gmail.com>
2024-10-08 15:39:43 +0600

    wishlist productcard issue solve

--- client/src/components/organisms/Product/ProductCard.tsx
+++ client/src/components/organisms/Product/ProductCard.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Swal from "sweetalert2";
 import Link from "next/link";
 import withReactContent from "sweetalert2-react-content";
+import toast from "react-hot-toast";
 
 import { useAuth } from "@/services/hooks/auth";
 import UseFetch from "@/services/hooks/useFetch";
@@ -49,29 +50,29 @@ const ProductCard: FC<ProductCardProps> = ({ product }) => {
       MySwal.fire("Error", "Something went wrong!", "error");
     }
   };
-
-  const handleRemoveFromWishlist = async () => {
-    const userWishList = wishlist?.find(
-      (wish: WishlistItem) =>
-        wish?.product?.id === product.id && wish.email === user?.email
-    );
-
-    if (!userWishList) return;
-
+  const email = user?.email;
+  const exactWishList = wishlist?.filter(
+    (wishListItem) => wishListItem?.productId === product.id
+  );
+  const userWishList = exactWishList?.find(
+    (wishListItem) => wishListItem?.email === email
+  );
+  const handleRemoveFromWishlist = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
     try {
       await axios.delete(
-        `https://api.korbojoy.shop/v1/wishlist/${userWishList.id}`
+        `https://api.korbojoy.shop/v1/wishlist/${userWishList?.id}`
       );
+      toast.success("Product removed from wishlist!");
       wishlistRefetch();
-    } catch (error) {
-      MySwal.fire("Error", "Unable to remove from wishlist", "error");
+    } catch (err) {
+      toast.error("Error removing product from wishlist");
     }
   };
 
-  const checkInWishlist = wishlist?.some(
-    (wish: WishlistItem) =>
-      wish?.product?.id === product.id && wish?.email === user?.email
-  );
+
 
   return (
     <Card
@@ -84,49 +85,50 @@ const ProductCard: FC<ProductCardProps> = ({ product }) => {
         New
       </span>
 
-      <Link href={`/productDetails/${product.id}`}>
-        <div
-          className="relative h-64 overflow-hidden m-4"
-          style={{ borderTopLeftRadius: "25px", borderTopRightRadius: "25px" }}
-        >
-          <Image
-            src={hover ? product.photos[1]?.src : product.photos[0]?.src}
-            alt={product.name}
-            layout="fill"
-            objectFit="cover"
-            className="absolute inset-0 transition-opacity duration-500 ease-in-out"
-          />
 
-          <div
-            className={`absolute inset-0 flex items-center justify-center transition-opacity duration-300 ease-in-out ${
-              hover ? "opacity-100" : "opacity-0"
+      <div
+        className="relative h-64 overflow-hidden m-4"
+        style={{ borderTopLeftRadius: "25px", borderTopRightRadius: "25px" }}
+      >
+
+        <Image
+          src={hover ? product.photos[1]?.src : product.photos[0]?.src}
+          alt={product.name}
+          layout="fill"
+          objectFit="cover"
+          className="absolute inset-0 transition-opacity duration-500 ease-in-out"
+        />
+
+        <div
+          className={`absolute inset-0 flex items-center justify-center transition-opacity duration-300 ease-in-out ${hover ? "opacity-100" : "opacity-0"
             }`}
-          >
-            <div className="relative flex space-x-4">
-              <Link href={`/account`} passHref>
-                <div className="relative group">
-                  <IconButton
-                    className="bg-[#e8f6ea] rounded-full p-2 shadow-md transition-transform duration-300 ease-in-out group-hover:bg-[#088178] group-hover:scale-110"
-                    onClick={
-                      checkInWishlist
-                        ? handleRemoveFromWishlist
-                        : handleAddToWishlist
-                    }
-                  >
-                    <Favorite
-                      className={`${
-                        checkInWishlist ? "text-red-500" : "text-[#088178]"
-                      } group-hover:text-white`}
-                    />
-                  </IconButton>
-                  <span className="absolute -mt-24 top-full left-1/2 transform -translate-x-1/2 bg-[#088178] text-white text-xs font-bold px-4 py-2 rounded transition-opacity duration-300 ease-in-out opacity-0 group-hover:opacity-100 w-[120px] text-center">
-                    {checkInWishlist
-                      ? "Remove from Wishlist"
-                      : "Add to Wishlist"}
-                    <span className="block absolute bottom-[-10px] left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-[10px] border-r-[10px] border-t-[10px] border-t-[#088178] border-l-transparent border-r-transparent"></span>
-                  </span>
-                </div>
-              </Link>
+        >
+          <div className="relative flex space-x-4">
+
+            <div className="relative group">
+              <IconButton
+                className={`bg-[#e8f6ea] rounded-full p-2 shadow-md transition-transform duration-300 ease-in-out  group-hover:scale-110 ${userWishList ? 'group-hover:bg-[#e8f6ea]' : "group-hover:bg-[#088178]"
+                  }`}
+                onClick={
+                  userWishList
+                    ? handleRemoveFromWishlist
+                    : handleAddToWishlist
+                }
+              >
+                <Favorite
+                  className={`${userWishList ? "text-red-500" : "text-[#088178]"
+                    } ${userWishList ? "group-hover:text-red-500" : "group-hover:text-white"
+                    } `}
+                />
+              </IconButton>
+              <span className="absolute -mt-24 top-full left-1/2 transform -translate-x-1/2 bg-[#088178] text-white text-xs font-bold px-4 py-2 rounded transition-opacity duration-300 ease-in-out opacity-0 group-hover:opacity-100 w-[120px] text-center">
+                {userWishList
+                  ? "Remove from Wishlist"
+                  : "Add to Wishlist"}
+                <span className="block absolute bottom-[-10px] left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-[10px] border-r-[10px] border-t-[10px] border-t-[#088178] border-l-transparent border-r-transparent"></span>
+              </span>
+            </div>
+            <Link href={`/productDetails/${product.id}`}>
               <div className="relative group">
                 <IconButton className="bg-[#e8f6ea] rounded-full p-2 shadow-md transition-transform duration-300 ease-in-out group-hover:bg-[#088178] group-hover:scale-110">
                   <Info className="text-[#088178] group-hover:text-white" />
@@ -136,10 +138,11 @@ const ProductCard: FC<ProductCardProps> = ({ product }) => {
                   <span className="block absolute bottom-[-10px] left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-[10px] border-r-[10px] border-t-[10px] border-t-[#088178] border-l-transparent border-r-transparent"></span>
                 </span>
               </div>
-            </div>
+            </Link>
           </div>
         </div>
-      </Link>
+      </div>
+
       <CardContent
         style={{
           borderBottomLeftRadius: "25px",
