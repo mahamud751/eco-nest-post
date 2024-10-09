f7930e43b873e0069b81d86baee65a9169e4fa0a
Mahamud Pino <mahamudpino24678@gmail.com>
2024-10-10 03:29:09 +0600

    review

--- client/src/app/(pages)/productDetails/[id]/page.tsx
+++ client/src/app/(pages)/productDetails/[id]/page.tsx
@@ -100,7 +100,11 @@ const ProductDetails = ({ params: { id } }: ProductDetailsProps) => {
         `${process.env.NEXT_PUBLIC_BASEURL}/v1/wishlist`,
         wishlistItem
       );
-      toast.success("Product added to wishlist!");
+      openSnackbar(
+        `${product?.name} Item successfully add to wishlist!`,
+        "success",
+        "#4caf50"
+      );
       wishlistRefetch();
     } catch (err) {
       toast.error("Error adding product to wishlist");
@@ -115,7 +119,11 @@ const ProductDetails = ({ params: { id } }: ProductDetailsProps) => {
       await axios.delete(
         `${process.env.NEXT_PUBLIC_BASEURL}/v1/wishlist/${userWishList?.id}`
       );
-      toast.success("Product removed from wishlist!");
+      openSnackbar(
+        `${product?.name} Item removed from wishlist!`,
+        "error",
+        "#4caf50"
+      );
       wishlistRefetch();
     } catch (err) {
       toast.error("Error removing product from wishlist");
@@ -194,128 +202,149 @@ const ProductDetails = ({ params: { id } }: ProductDetailsProps) => {
             </div>
           </div>
 
-          <div>
-            <h1 className="text-3xl font-bold mb-2">{product?.name}</h1>
-            <div className="flex">
-              <StarRatings
-                rating={averageRating}
-                starRatedColor="gold"
-                numberOfStars={5}
-                starDimension="20px"
-                starSpacing="2px"
-                name="averageRating"
-              />
-              <p className="ms-3 mt-[2px]">({averageRating.toFixed(1)} / 5)</p>
-            </div>
-            <p className="font-bold my-4"> Category: {categoryName}</p>
-            <p className="text-gray-600 mb-6">{product?.desc}</p>
-            <Box className="flex items-center space-x-2 mb-6">
-              <VisibilityIcon className="text-[#286156]" />
-              <Typography
-                variant="body1"
-                className="text-[#4a69b8] font-semibold"
-              >
-                {product?.views} Users Recently Viewed This Product
+          <Card className="shadow-lg rounded-lg p-5 h-[520px]">
+            <CardContent>
+              <h1 className="text-3xl font-bold mb-2">{product?.name}</h1>
+              <div className="flex items-center mb-4">
+                <StarRatings
+                  rating={averageRating}
+                  starRatedColor="gold"
+                  numberOfStars={5}
+                  starDimension="20px"
+                  starSpacing="2px"
+                  name="averageRating"
+                />
+                <p className="ms-3 mt-[2px]">
+                  ({averageRating.toFixed(1)} / 5)
+                </p>
+              </div>
+              <Typography variant="h6" className="font-bold my-4">
+                Category: {categoryName}
               </Typography>
-            </Box>
-
-            {colors.length > 0 && (
-              <div className="mb-4">
-                <p className="text-lg font-semibold mb-2">Select Color</p>
-                <div className="flex gap-4">
-                  {colors.map((clr) => (
-                    <button
-                      key={clr}
-                      className={`w-8 h-8 rounded-full border-2 ${
-                        color === clr ? "border-blue-500" : "border-gray-300"
-                      }`}
-                      style={{ backgroundColor: clr }}
-                      onClick={() => setColor(clr)}
-                    ></button>
-                  ))}
-                </div>
-              </div>
-            )}
-
-            {sizes.length > 0 && (
-              <div className="mb-4">
-                <p className="text-lg font-semibold mb-2">Select Size</p>
-                <div className="flex gap-2">
-                  {sizes.map((sz) => (
-                    <button
-                      key={sz}
-                      className={`w-10 h-10 text-sm font-bold border-2 ${
-                        size === sz
-                          ? "border-blue-500 bg-blue-100"
-                          : "border-gray-300"
-                      } rounded-lg`}
-                      onClick={() => setSize(sz)}
-                    >
-                      {sz}
-                    </button>
-                  ))}
-                </div>
-              </div>
-            )}
-
-            <div className="flex items-center">
-              <Button
-                variant="outlined"
-                className="flex items-center justify-between w-20 border-gray-400 text-black p-1 h-10"
-              >
-                <span className="flex-grow text-center text-black">
-                  {quantity}
-                </span>
-
-                <div className="flex flex-col ml-1">
-                  <KeyboardArrowUpIcon
-                    onClick={() => setQuantity((prev) => prev + 1)}
-                    className="text-black"
-                  />
-                  <KeyboardArrowDownIcon
-                    onClick={() => setQuantity((prev) => Math.max(1, prev - 1))}
-                    className="text-black mt-[-4px]"
-                  />
-                </div>
-              </Button>
-              <Button
-                variant="contained"
-                onClick={() =>
-                  product &&
-                  handleAddItem({
-                    product,
-                    quantity: quantity,
-                    size: size || "N/A",
-                    color: color || "N/A",
-                  })
-                }
-                className="bg-[#088178] px-5 ms-3 shadow-md transition-transform duration-300 ease-in-out group-hover:scale-110 h-10"
-                disabled={!product}
-              >
-                Add to Cart
-              </Button>
-
-              <Button
-                variant="outlined"
-                className="ms-2 border-gray-400 h-10 w-1.5 "
-              >
-                <IconButton
-                  onClick={
-                    userWishList
-                      ? handleRemoveFromWishlist
-                      : handleAddToWishlist
-                  }
-                  className="focus:outline-none"
+              <Typography variant="body1" className="text-gray-600 mb-4">
+                {product?.desc}
+              </Typography>
+              <Box className="flex items-center space-x-2 mb-6">
+                <VisibilityIcon className="text-[#286156]" />
+                <Typography
+                  variant="body1"
+                  className="text-[#4a69b8] font-semibold"
                 >
-                  <FavoriteIcon
-                    className={`transition-all duration-300 ${
-                      userWishList ? "text-red-500" : "text-gray-300"
-                    }`}
-                  />
-                </IconButton>
-              </Button>
-            </div>
-          </div>
+                  {product?.views} Users Recently Viewed This Product
+                </Typography>
+              </Box>
+
+              {colors.length > 0 && (
+                <div className="mb-4">
+                  <Typography
+                    variant="body1"
+                    className="text-lg font-semibold mb-2"
+                  >
+                    Select Color
+                  </Typography>
+                  <div className="flex gap-4">
+                    {colors.map((clr) => (
+                      <button
+                        key={clr}
+                        className={`w-8 h-8 rounded-full border-2 transition-all duration-300 ${
+                          color === clr
+                            ? "border-blue-500 ring-2 ring-blue-200"
+                            : "border-gray-300"
+                        }`}
+                        style={{ backgroundColor: clr }}
+                        onClick={() => setColor(clr)}
+                      ></button>
+                    ))}
+                  </div>
+                </div>
+              )}
+
+              {sizes.length > 0 && (
+                <div className="mb-4">
+                  <Typography
+                    variant="body1"
+                    className="text-lg font-semibold mb-2"
+                  >
+                    Select Size
+                  </Typography>
+                  <div className="flex gap-2">
+                    {sizes.map((sz) => (
+                      <button
+                        key={sz}
+                        className={`w-10 h-10 text-sm font-bold border-2 transition-all duration-300 ${
+                          size === sz
+                            ? "border-blue-500 bg-blue-100"
+                            : "border-gray-300"
+                        } rounded-lg`}
+                        onClick={() => setSize(sz)}
+                      >
+                        {sz}
+                      </button>
+                    ))}
+                  </div>
+                </div>
+              )}
+
+              <div className="flex items-center mb-4">
+                <Button
+                  variant="outlined"
+                  className="flex items-center justify-between w-20 border-gray-400 text-black p-1 h-10"
+                >
+                  <span className="flex-grow text-center text-black">
+                    {quantity}
+                  </span>
+                  <div className="flex flex-col ml-1">
+                    <KeyboardArrowUpIcon
+                      onClick={() => setQuantity((prev) => prev + 1)}
+                      className="text-black cursor-pointer"
+                    />
+                    <KeyboardArrowDownIcon
+                      onClick={() =>
+                        setQuantity((prev) => Math.max(1, prev - 1))
+                      }
+                      className="text-black cursor-pointer mt-[-4px]"
+                    />
+                  </div>
+                </Button>
+                <Button
+                  variant="contained"
+                  onClick={() =>
+                    product &&
+                    handleAddItem({
+                      product,
+                      quantity,
+                      size: size || "N/A",
+                      color: color || "N/A",
+                    })
+                  }
+                  className="bg-[#088178] px-5 ms-3 shadow-md transition-transform duration-300 ease-in-out hover:scale-110 h-10"
+                  disabled={!product}
+                >
+                  Add to Cart
+                </Button>
+
+                <Button
+                  variant="outlined"
+                  className="ms-2 border-gray-400 h-10"
+                >
+                  <IconButton
+                    onClick={
+                      userWishList
+                        ? handleRemoveFromWishlist
+                        : handleAddToWishlist
+                    }
+                    className="focus:outline-none"
+                  >
+                    <FavoriteIcon
+                      className={`transition-all duration-300 ${
+                        userWishList ? "text-red-500" : "text-gray-300"
+                      }`}
+                    />
+                  </IconButton>
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
           {product && <AdditionalTab product={product} />}
         </div>
 
