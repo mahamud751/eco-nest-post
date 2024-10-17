9346e09ef00773a01ae1aef2fcb509abe1d28bc8
Mahamud Pino <mahamudpino24678@gmail.com>
2024-10-17 13:52:33 +0600

    add notification

--- server/src/order/order.module.ts
+++ server/src/order/order.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { NotificationService } from 'src/notification/notification.service';
 
 @Module({
   controllers: [OrderController],
-  providers: [OrderService, PrismaService],
+  providers: [OrderService, PrismaService, NotificationService],
 })
-export class OrderModule {}
+export class OrderModule { }
