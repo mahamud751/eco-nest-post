9f48f6a56d6fd5ddd36fa562cd5a0881a4ffbff2
Mahamud Pino <mahamudpino24678@gmail.com>
2024-10-22 15:30:41 +0600

    react-odometerjs

--- client/src/components/templates/shared/CartSummary.tsx
+++ client/src/components/templates/shared/CartSummary.tsx
@@ -1,29 +1,59 @@
-'use client'
+'use client';
+import { useState } from "react";
 import { Box, Typography } from "@mui/material";
 import { ShoppingCart as CartIcon } from "@mui/icons-material";
 import Odometer from "react-odometerjs";
 import { useSelector } from "react-redux";
 import { RootState } from "@/app/redux/reducers";
 import "odometer/themes/odometer-theme-default.css";
+import CartDrawer from "./CartDrawer";
+import { useSnackbar } from "@/services/contexts/useSnackbar";
+import { useAppDispatch } from "@/services/hooks/useAppDispatch";
+import { delete_item } from "@/app/redux/actions/cartAction";
 
 const CartSummary = () => {
+    const [cartOpen, setCartOpen] = useState(false);
+    const { openSnackbar } = useSnackbar();
+    const dispatch = useAppDispatch();
     const cartItemsFromRedux = useSelector(
         (state: RootState) => state.cart.cartItems
     );
+
+    const toggleCart = () => setCartOpen(!cartOpen);
+
     const grandTotal = cartItemsFromRedux
         .reduce((acc, item) => acc + item.product.price * item.quantity, 0)
         .toFixed(2);
 
     const cartItemCount = cartItemsFromRedux.length;
 
+    const removeItem = (index: number) => {
+        if (index < 0 || index >= cartItemsFromRedux.length) return;
+        const item = cartItemsFromRedux[index];
+        if (item) {
+            dispatch(delete_item(item.product.id, item.size, item.color));
+            openSnackbar(
+                `${item.product.name} Item removed from cart!`,
+                "success",
+                "#f44336"
+            );
+        }
+    };
+
     return (
-        <Box position="fixed"
+        <Box
+            position="fixed"
             top="50%"
-            right={20}
+            right={1}
             zIndex={1000}
-            display="flex"
-            justifyContent="center"
-            alignItems="center">
+            sx={{
+                cursor: 'pointer',
+                '&:hover': {
+                    opacity: 0.8,
+                }
+            }}
+            onClick={toggleCart}
+        >
             <div className="text-center text-white">
                 <div className="p-2 bg-[#126963] rounded-tl-[25%]">
                     <div>
@@ -41,6 +71,7 @@ const CartSummary = () => {
                     <Odometer value={parseFloat(grandTotal)} format="d" theme="default" />
                 </div>
             </div>
+            <CartDrawer open={cartOpen} onClose={toggleCart} removeItem={removeItem} />
         </Box>
     );
 };
--- client/src/components/templates/shared/Navbar.tsx
+++ client/src/components/templates/shared/Navbar.tsx
@@ -28,8 +28,6 @@ import {
   ContactMail as ContactIcon,
   AccountCircle as AccountIcon,
   Facebook,
-  Delete,
-  Close,
   Home,
   Info,
   Category as CategoryIcon,
@@ -49,6 +47,7 @@ import { useSnackbar } from "@/services/contexts/useSnackbar";
 import { Category } from "@/services/types/types";
 import UseFetch from "@/services/hooks/useFetch";
 import { useSession } from "next-auth/react";
+import CartDrawer from "./CartDrawer";
 
 export default function Navbar() {
   const { user, logoutUser } = useAuth();
@@ -513,104 +512,7 @@ export default function Navbar() {
           </List>
         </div>
       </Drawer>
-      <Drawer anchor="right" open={cartOpen} onClose={toggleCart}>
-        <div className="w-80 p-6">
-          <div className="flex justify-between">
-            <h2 className="text-2xl font-semibold text-gray-800">Your Cart</h2>
-            <div className="bg-red-500 p-1">
-              <Close
-                className="cursor-pointer text-white"
-                onClick={toggleCart}
-              />
-            </div>
-          </div>
-          <hr className="mt-5" />
-
-          <div className="mt-6 space-y-4">
-            {cartItemsFromRedux.length > 0 ? (
-              cartItemsFromRedux.map((item, index) => (
-                <Link href={`/productDetails/${item?.product?.id}`} key={index}>
-                  <div
-                    key={index}
-                    className="flex items-center justify-between p-3"
-                  >
-                    {item?.product?.photos?.length > 0 ? (
-                      <Image
-                        width={50}
-                        height={50}
-                        src={item?.product?.photos[0]?.src}
-                        alt={item?.product?.name}
-                        className="rounded-md object-cover"
-                      />
-                    ) : (
-                      <span className="text-gray-500 italic">No Image</span>
-                    )}
-
-                    <div className="flex-1 mx-3">
-                      <p className="font-medium text-gray-800">
-                        {item?.product?.name}
-                      </p>
-                      <div className="text-sm text-gray-600">
-                        <span>{item?.quantity}</span> x{" "}
-                        <span>৳{item?.product?.price}</span>
-                      </div>
-                    </div>
-
-                    <IconButton
-                      onClick={() => removeItem(index)}
-                      aria-label="delete"
-                      color="error"
-                      className="hover:bg-red-100"
-                    >
-                      <Delete />
-                    </IconButton>
-                  </div>
-                </Link>
-              ))
-            ) : (
-              <p className="text-center text-gray-500">Your cart is empty.</p>
-            )}
-          </div>
-        </div>
-        <hr />
-        <div className="flex justify-between my-5 px-8">
-          <p>SubTotal:</p>
-          <p className="text-red-500 font-bold">
-            ৳
-            {cartItemsFromRedux
-              .reduce(
-                (acc, item) => acc + item.product?.price * item?.quantity,
-                0
-              )
-              .toFixed(2)}
-          </p>
-        </div>
-        <hr />
-        <div className="uppercase p-3">
-          <div className="mt-6">
-            <Link href={"/cart"}>
-              <Button
-                variant="contained"
-                fullWidth
-                className="py-3 rounded-lg bg-[#088178]"
-              >
-                View Cart
-              </Button>
-            </Link>
-          </div>
-          <div className="mt-6">
-            <Link href={"/cart"}>
-              <Button
-                variant="contained"
-                fullWidth
-                className="py-3 rounded-lg bg-[#088178]"
-              >
-                Checkout
-              </Button>
-            </Link>
-          </div>
-        </div>
-      </Drawer>
+      <CartDrawer open={cartOpen} onClose={toggleCart} removeItem={removeItem} />
     </>
   );
 }
