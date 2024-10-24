2fa86a7af549225d5cacefccc911f691c91d7b87
Mahamud Pino <mahamudpino24678@gmail.com>
2024-10-24 17:07:24 +0600

    add rider

---
+++ admin/src/app/(pages)/rider-list/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+import DataTable from "@/components/templates/DataTable";
+import { getCommonColumns } from "@/components/templates/CommonColums";
+
+const RiderList = () => {
+  const columns = getCommonColumns([
+    { field: "email", headerName: "Email", flex: 1 },
+    { field: "phone", headerName: "Phone", flex: 1 },
+    { field: "role", headerName: "Role", flex: 1 },
+  ]);
+
+  return (
+    <DataTable
+      fetchUrl={`${process.env.NEXT_PUBLIC_BASEURL}/v1/users?role=rider`}
+      deleteUrl={`${process.env.NEXT_PUBLIC_BASEURL}/v1/users`}
+      columns={columns}
+      searchField="name"
+      // defaultHiddenColumns={["id"]}
+      link="user-list"
+    />
+  );
+};
+
+export default RiderList;
