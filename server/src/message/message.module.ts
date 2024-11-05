ff3cb43fabfb9907c21673faac32a95a18f8363e
Mahamud Pino <mahamudpino24678@gmail.com>
2024-11-05 21:54:05 +0600

    add message server

--- server/src/message/message.module.ts
+++ server/src/message/message.module.ts
@@ -2,9 +2,11 @@
 import { Module } from '@nestjs/common';
 import { MessagesGateway } from './message.getway';
 import { MessagesService } from './message.service';
+import { MessagesController } from './message.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [MessagesGateway, MessagesService],
-  exports: [MessagesService],
+  controllers: [MessagesController],
+  providers: [MessagesGateway, PrismaService, MessagesService],
 })
 export class MessagesModule {}
