cd59a3eb20e649a5a0f8766a656206362a824923
Mahamud Pino <mahamudpino24678@gmail.com>
2024-11-21 18:27:35 +0600

    server swager api change docs

--- server/src/main.ts
+++ server/src/main.ts
@@ -48,7 +48,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
